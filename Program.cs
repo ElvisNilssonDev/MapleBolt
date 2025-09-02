e003dc28e72fc6faff97b8034057be1c620f6b4d
ElvisNilssonDev <Nilssonelvis@gmail.com>
2025-09-02 14:28:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MapleBolt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to MapleBolt");
+            Console.ReadLine();
+        }
+    }
+}
